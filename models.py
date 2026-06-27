414a32d46b99c8f670abba73c5756a0b4f57b1af
Flavio <flavio.giacomozzi97@gmail.com>
2026-06-27 23:56:36 +0200

    Implementing classes

--- models.py
+++ models.py
@@ -1,4 +1,5 @@
 from enum import Enum
+import datetime
 
 class MatchStatus(Enum):
     DA_INIZIARE = 1
@@ -20,22 +21,31 @@ class Team():
         self.win = 0
         self.lost = 0
         self.draws = 0
+    def get_points(self):
+        return self.points
+    def set_points(self, points):
+        self.points = points
+    def add_points(self, points):
+        self.points += points
     def add_player(self, player):
         self.players.append(player)
     def remove_player(self, player):
         self.players.remove(player)
-    def add_points(self, points):
-        self.points += points
+    
     
 
 class Match():
     def __init__(self, team_a, team_b):
-        self.date
-        self.team_a_pts = 0
-        self.team_b_pts = 0
+        self.date = datetime.date.today()
         self.team_a = team_a
         self.team_b = team_b
+        self.team_a_pts = self.team_a.get_points()
+        self.team_b_pts = self.team_b.get_points()
         self.status = MatchStatus.DA_INIZIARE
+    def get_points(self):
+        return [self.team_a_pts, self.team_b_pts]
+    def set_points(self, points):
+        self.points = points
     def add_points(self, points_a, points_b):
         self.team_a.add_points(points_a)
         self.team_b.add_points(points_b)
